00e970913dcce4785b91fd02bb005886a401b68d
Tony Bostony <145413690+neurocosm@users.noreply.github.com>
2026-08-30 21:52:19 -0400

    UPDATED FONT weight

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // NOMAD Central Version Registry
 // Edit this single line at the end of a session to update the version across all pages and modals.
-window.NOMAD_VERSION = "v3.08302026.1037";
+window.NOMAD_VERSION = "v3.08302026.1049";
 
 function applyNomadVersion() {
   const version = window.NOMAD_VERSION || "v3.0";
